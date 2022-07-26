eca174fd1e2832c2d79ceae7133db0bf58edafcd
Desh Iyer <desh.iyer@gmail.com>
2022-07-26 16:15:02 +0530

    Added code from way back when

---
+++ relics-of-the-past/personal/Classes - 1.cpp
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<stdio.h>
+#include<conio.h>
+
+int i;
+
+class STUDENT
+	{
+   	private :
+
+      int rno;
+      char name[10];
+      float marks[5];
+      float total;
+      void calculate
+      	{
+          total = 0;
+          for(i=0;i<5;++i)
+          	total+=
+         }
+
+      public :
+
+
+   }
