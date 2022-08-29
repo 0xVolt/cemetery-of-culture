f3d224523f8ab5f123f2f1439ce8d7290bd1c43f
Desh Iyer <desh.iyer@gmail.com>
2022-08-29 17:42:16 +0530

    Fomatted main.py

--- divy's-code/serebro/main.py
+++ divy's-code/serebro/main.py
@@ -1,120 +1,118 @@
+import pickle
+import json
+import random
+import tensorflow
+import tflearn
+from tensorflow.python.framework import ops
+import numpy
 from typing import List
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
-stemmer= LancasterStemmer()
-import numpy
-from tensorflow.python.framework import ops
+stemmer = LancasterStemmer()
 
-import tflearn
-import tensorflow
-import random
-import json
-import pickle
 with open("intents.json") as file:
-    data=json.load(file)
+    data = json.load(file)
 
 
-words= []
-labels= []
-docs_x= []
-docs_y= []
+words = []
+labels = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
-        wrds= nltk.word_tokenize(pattern)
+        wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
         docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
-            labels.append(intent["tag"])
-    
-try:
-    
-    with open("data.pickle", "rb") as f:
-        words,labels,training,output = pickle.load(f)
+        labels.append(intent["tag"])
 
+try:
+
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
 
 
 except:
 
-    words= [stemmer.stem(w.lower())for w in words if w != "?"]
-    words= sorted(list(set(words)))
+    words = [stemmer.stem(w.lower())for w in words if w != "?"]
+    words = sorted(list(set(words)))
     labels = sorted(labels)
-    training= []
-    output= []
+    training = []
+    output = []
 
     out_empty = [0 for _ in range(len(labels))]
 
     for x, doc in enumerate(docs_x):
-        bag=[]
-        wrds= [stemmer.stem(w) for w in doc]
-        
+        bag = []
+        wrds = [stemmer.stem(w) for w in doc]
+
         for w in words:
             if w in wrds:
                 bag.append(1)
             else:
                 bag.append(0)
-        
+
         output_row = out_empty[:]
         output_row[labels.index(docs_y[x])] = 1
         training.append(bag)
         output.append(output_row)
 
-    training= numpy.array(training)
-    output= numpy.array(output)
+    training = numpy.array(training)
+    output = numpy.array(output)
     with open("data.pickle", "wb") as f:
-        pickle.dump((words,labels,training,output), f)
+        pickle.dump((words, labels, training, output), f)
 
 
 ops.reset_default_graph()
 
-net= tflearn.input_data(shape=[None, len(training[0])])
-net= tflearn.fully_connected(net, 8)
-net= tflearn.fully_connected(net, 8)
-net= tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net= tflearn.regression(net)
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
 model = tflearn.DNN(net)
-
 
 
 model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 model.save("model.tflearn")
 
 
-def bag_of_words(s,words):
-    bag=[0 for _ in range(len(words))]
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
     s_words = nltk.word_tokenize(s)
-    s_words= [stemmer.stem(word.lower()) for word in s_words]
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
 
     for se in s_words:
-        for i,w in enumerate(words):
-            if w==se:
-                bag[i]=1
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
 
     return numpy.array(bag)
+
 
 def chat():
     print("start talking with the bot!")
     while True:
-        inp= input("you: ")
-        if inp.lower()=="quit":
+        inp = input("you: ")
+        if inp.lower() == "quit":
             break
 
         result = model.predict([bag_of_words(inp, words)])[0]
-        result_index= numpy.argmax(result)
+        result_index = numpy.argmax(result)
         tag = labels[result_index]
-        
-        if result[result_index]>0.7:
+
+        if result[result_index] > 0.7:
 
             for tg in data["intents"]:
-                if tg['tag']==tag:
+                if tg['tag'] == tag:
                     responses = tg['responses']
 
             print(random.choice(responses))
         else:
             print("I didn't understand you, try again :D")
 
+
 chat()
-
-
