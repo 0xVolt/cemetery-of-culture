b4e06dcabc79155711b18a05d2e1733dcfde4812
Desh Iyer <desh.iyer@gmail.com>
2022-09-18 15:50:31 +0530

    Updated example graph

--- year-3/algorithms-for-intelligent-systems/Lab Test 1 - 12-8-22/2-dfs.py
+++ year-3/algorithms-for-intelligent-systems/Lab Test 1 - 12-8-22/2-dfs.py
@@ -1,17 +1,17 @@
 # Using a Python dictionary to act as an adjacency list
 G = {
     '0': ['1', '2'],
-    '1': ['3', '4'],
-    '2': [],
+    '1': ['3'],
+    '2': ['3', '4'],
     '3': [],
-    '4': ['2']
+    '4': []
 }
 
 visited = set() # Set to keep track of visited nodes of graph.
 
 def dfs(visited, graph, node):  #function for dfs 
     if node not in visited:
-        print (node)
+        print(node)
         visited.add(node)
         for neighbour in graph[node]:
             dfs(visited, graph, neighbour)
