7ade796d68fa58b30e6f3e3b2f626ac6fa0771da
Desh Iyer <desh.iyer@gmail.com>
2022-11-24 00:51:53 +0530

    Updated download.py

--- year-3/neural-networks/5-cnns/download.py
+++ year-3/neural-networks/5-cnns/download.py
@@ -21,24 +21,19 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                             download=True, transform=transform)
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                            download=True, transform=transform)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
-                                           shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
-                                          shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
 def imshow(img):
-    img = img / 2 + 0.5  # unnormalize
+    img = img / 2 + 0.5  # Un-normalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
