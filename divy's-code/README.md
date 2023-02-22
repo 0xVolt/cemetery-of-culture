14c666e1479c4434a574318233dbb0be01f5571f
Desh Iyer <desh.iyer@gmail.com>
2023-02-22 22:13:14 +0530

    Remove 'Divy's Code'/
    Delete useless files that add to repos memory footprint
    Made sure all the code is backed up elsewhere :)

--- divy's-code/README.md
+++
@@ -1,2 +0,0 @@
-## Bhaiya... advance. Badi.
----
--- divy's-code/serebro/load.py
+++
@@ -1,4 +0,0 @@
-with open(r"C:\Users\deshi\Code\cemetery-of-culture\divy's-code\serebro\intents.json", 'r') as f:
-    l = f.readlines()
-
-print(l)
