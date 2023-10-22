9ab4804fee7fb6224d23932ee13ed015bdaa8566
Desh Iyer <desh.iyer@gmail.com>
2023-10-22 18:47:36 +0530

    Import `checkGPU.py` script

--- checkGPU.py
+++ checkGPU.py
@@ -1,5 +1,9 @@
 import torch
+# print(torch.cuda.device_count())
+# print(torch.cuda.current_device())
 
 print('Checking for GPU...')
-
 print(f'{torch.cuda.is_available()}')
+
+# import tensorflow as tf
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
